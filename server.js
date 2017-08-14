a97ec08b26aaf7421fd67b12da6ff6393fe02328
Deepakkv <deepakkv@in-deepakkv.local>
2017-08-14 21:47:40 +0530

    [Deepak] node auth initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// required for passport
+app.use(session({ secret: 'secert' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
